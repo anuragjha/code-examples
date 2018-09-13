f11aa45676d6b88b681ea219125da1c2349538e3
Sami Rollins <srollins@cs.usfca.edu>
2018-09-13 13:56:05 -0700

    update example

--- Sockets/src/YelpClient.java
+++ Sockets/src/YelpClient.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 import java.util.Map;
 
@@ -8,13 +12,25 @@ public class YelpClient {
 
 	final static int PORT = 80;
 
+	//propagate exception to simplify demonstration code
 	public static void main(String[] args) throws Exception {
-		
+
+		//create URL object
 		URL url = new URL("https://www.yelp.com/biz/the-velo-rouge-cafe-san-francisco");
+
+		//create secure connection 
 		HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
-		connection.setRequestMethod("GET");
+		//set HTTP method
+		connection.setRequestMethod("GET");		
 		connection.connect();
-		
+
+		printHeaders(connection);
+
+		printBody(connection);
+
+	}
+
+	public static void printHeaders(URLConnection connection) {
 		Map<String,List<String>> headers = connection.getHeaderFields();
 		for(String key: headers.keySet()) {
 			System.out.println(key);
@@ -22,15 +38,15 @@ public class YelpClient {
 			for(String value: values) {
 				System.out.println("\t" + value);
 			}
-			
-			
-		}
-//		BufferedReader reader = new BufferedReader(
-//				new InputStreamReader(connection.getInputStream()));
-//		String line;
-//		while((line = reader.readLine()) != null) {
-//			System.out.println(line);
-//		}
-		
+		}		
+	}
+
+	public static void printBody(URLConnection connection) throws IOException {
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(connection.getInputStream()));
+		String line;
+		while((line = reader.readLine()) != null) {
+			System.out.println(line);
+		} 
 	}
 }
